c856a3eba067c020e82105931a4ba588fb374f53
Develru <develru@outlook.com>
2014-06-12 20:57:28 +0200

    Tool window is added

--- controlcenter.py
+++ controlcenter.py
@@ -3,8 +3,10 @@ __author__ = 'develru'
 from PySide import QtGui
 import pyMainWindow
 
+
 class ControlWindow(QtGui.QMainWindow, pyMainWindow.Ui_MainWindow):
 
     def __init__(self, parent=None):
         super(ControlWindow, self).__init__(parent)
         self.setupUi(self)
+        self.mdiArea.addSubWindow(self.toolWindow)
