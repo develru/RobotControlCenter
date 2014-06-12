9bfd07a0aa92a83378e51b38232c8fd93f4e178d
Develru <develru@outlook.com>
2014-06-12 21:24:16 +0200

    Ui files generated

---
+++ uiToolWindow.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '.\toolWindow.ui'
+#
+# Created: Thu Jun 12 21:23:22 2014
+#      by: pyside-uic 0.2.15 running on PySide 1.2.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore, QtGui
+
+class Ui_toolWin(object):
+    def setupUi(self, toolWin):
+        toolWin.setObjectName("toolWin")
+        toolWin.resize(255, 300)
+        self.verticalLayout = QtGui.QVBoxLayout(toolWin)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.connectBtn = QtGui.QPushButton(toolWin)
+        self.connectBtn.setObjectName("connectBtn")
+        self.verticalLayout.addWidget(self.connectBtn)
+        self.activateCamBtn = QtGui.QPushButton(toolWin)
+        self.activateCamBtn.setObjectName("activateCamBtn")
+        self.verticalLayout.addWidget(self.activateCamBtn)
+        spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem)
+
+        self.retranslateUi(toolWin)
+        QtCore.QMetaObject.connectSlotsByName(toolWin)
+
+    def retranslateUi(self, toolWin):
+        toolWin.setWindowTitle(QtGui.QApplication.translate("toolWin", "Tools", None, QtGui.QApplication.UnicodeUTF8))
+        self.connectBtn.setText(QtGui.QApplication.translate("toolWin", "Connect", None, QtGui.QApplication.UnicodeUTF8))
+        self.activateCamBtn.setText(QtGui.QApplication.translate("toolWin", "Activate the camera", None, QtGui.QApplication.UnicodeUTF8))
+
