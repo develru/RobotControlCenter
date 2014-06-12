3e67c6becc2abdd1490cc6d18ad8601ea6da4f16
Develru <develru@outlook.com>
2014-06-12 21:35:04 +0200

    The tool window is separated in a new class.

--- controlcenter.py
+++ controlcenter.py
@@ -2,6 +2,7 @@ __author__ = 'develru'
 
 from PySide import QtGui, QtCore
 import uiMainWindow
+import uiToolWindow
 
 
 class ControlWindow(QtGui.QMainWindow, uiMainWindow.Ui_MainWindow):
@@ -9,5 +10,15 @@ class ControlWindow(QtGui.QMainWindow, uiMainWindow.Ui_MainWindow):
     def __init__(self, parent=None):
         super(ControlWindow, self).__init__(parent)
         self.setupUi(self)
-        self.mdiArea.addSubWindow(self.toolWindow)
-        self.toolWindow.setWindowFlags(QtCore.Qt.CustomizeWindowHint | QtCore.Qt.WindowMinMaxButtonsHint)
+        tool_win = ToolWindow()
+        self.mdiArea.addSubWindow(tool_win)
+
+
+class ToolWindow(QtGui.QWidget, uiToolWindow.Ui_toolWin):
+    def __init__(self, parent=None):
+        super(ToolWindow, self).__init__(parent)
+        self.setupUi(self)
+
+    def closeEvent(self, event):
+        event.ignore()
+        QtGui.QMessageBox.warning(self, 'Warning', 'The tool window is not to close!')
