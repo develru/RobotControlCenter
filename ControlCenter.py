9196f053edccffc13cb86420c4f219d002a37e71
Develru <develru@outlook.com>
2014-06-12 20:40:56 +0200

    Main window added

---
+++ ControlCenter.py
@@ -0,0 +1,10 @@
+__author__ = 'develru'
+
+from PySide import QtGui
+import pyMainWindow
+
+class ControlWindow(QtGui.QMainWindow, pyMainWindow.Ui_MainWindow):
+
+    def __init__(self, parent=None):
+        super(ControlWindow, self).__init__(parent)
+        self.setupUi(self)
